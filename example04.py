c55d16c311270283db48101bb2f2167abb5278f9
shishaoquan <shishaoquan@126.com>
2022-05-10 14:02:32 +0800

    第 5 次 提交测试一下

--- example04.py
+++ example04.py
@@ -8,3 +8,12 @@ example04 -
 
 
 """
+
+print('Welcome to the tip calculator.')
+total_bill = input('What was the total bill? ')
+percentage = input('What percentage tip would you like to give ? 10, 12, or 15? ')
+person_num = input('How many people to split the bill? ')
+last_bill = float(total_bill) * (100 + int(percentage))/100
+every_cost = last_bill / int(person_num)
+last_str = f'Each person should pay: {every_cost:.2f}'
+print(last_str)
