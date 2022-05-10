87d41f3c4a9a792f6522b7e6cbc2c86552f38339
石少全 <shishaoquan>
2022-05-10 22:09:53 +0800

    在 家里提交

--- example08.py
+++ example08.py
@@ -12,7 +12,7 @@ example08 -
 year = int(input('Please enter the year you want to check. '))
 if year % 4 == 0:
     if year % 100 == 0:
-        if year % 400:
+        if year % 400 == 0:
             print(f'{year} is leap year.')
 else:
     print(f'{year} is not a leap year')
